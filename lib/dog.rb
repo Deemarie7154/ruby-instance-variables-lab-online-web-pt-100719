fb08fb22bd997a36f17a0b3df846507391912168
Dwight Marie <dwightmarie@gmail.com>
2019-10-27 00:16:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,6 @@
 class Dog 
   def name=(dog_name)
       this_dogs_name = dog_name
-      lassie = Dog.new
-lassie.name = "Lassie"
  
 lassie.name 
 end
@@ -12,3 +10,7 @@ def name
   @this_dogs_name
 end
 end
+
+     lassie = Dog.new
+lassie.name = "Lassie"
+ puts lassie.name
